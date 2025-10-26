1ab873b67c9bc54a07e3c575b6403a27314566ba
Jonilson Sousa <jonilsonds9@gmail.com>
2025-10-26 16:16:50 -0300

    Adicionando projeto base

---
+++ internal/infra/graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/jonilsonds9/goexpert-desafio-3-clean-architecture/internal/usecase"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	CreateOrderUseCase usecase.CreateOrderUseCase
+}
